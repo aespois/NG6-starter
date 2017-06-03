6fffa55c25bcd4e7539084fb0e10e940afc4ca4b
menezes <menezes.ime@gmail.com>
2017-06-03 16:51:40 -0300

    uib-example v1.0

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,7 +1,10 @@
 import angular from 'angular';
+import navbarModule from './navbar/navbar';
+import footerModule from './footer/footer';
 
 let commonModule = angular.module('app.common', [
-
+  navbarModule,
+  footerModule
 ])
 
 .name;
