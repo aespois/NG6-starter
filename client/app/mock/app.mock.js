1abab07a6bd4fbbec8dc6671fd5abae3a6b302c0
menezes <menezes.ime@gmail.com>
2017-06-05 15:48:28 -0300

    Adicionado mais alguns exemplos de operações

--- client/app/mock/app.mock.js
+++ client/app/mock/app.mock.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMocks from 'angular-mocks';
 import appModule from '../app';
+import storageService from './storage.service';
 import testMock from './test/test';
 import contactsMock from './contacts/contacts';
 
@@ -8,6 +9,8 @@ let appMockModule = angular.module('appMock', [
   appModule,
   'ngMockE2E'
 ])
+
+.service('StorageService', storageService)
 
 .run(testMock)
 .run(contactsMock)
--- client/app/mock/test/test.js
+++ client/app/mock/test/test.js
@@ -1,7 +1,7 @@
 function TestMock($httpBackend, $log) {
 
   $httpBackend.whenGET('/api/test').respond( (method, url, data, headers) => {
-    $log.debug('Dados recebidos:', method, url, data, headers);
+    $log.log('Dados recebidos:', method, url, data, headers);
     return [200, 'Testando o HTTTP/GET.', {}];
   });
 
